1403af44b744612f45abf63c6141421fd59f808d
Th0m4sAnd3rs0n <cation-tubulaire0m@icloud.com>
2023-10-16 17:05:25 +0200

    Ajout d'une fenètre modale

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Skydata website v 0.1
+Skydata website / Frontend only
 
 Page principale : Mozaïque avec une carte par dropzone
+
+Affichage d'une fenètre modale lorsque l'on clique sur une dropzone 
+
+
--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function Redirection(path) {
     location.href = path;
 }
 
+function openModal(url) {
+    const modal = document.getElementById("myModal");
+    const modalIframe = document.getElementById("modal-iframe");
+    modalIframe.src = url;
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    const modal = document.getElementById("myModal");
+    const modalIframe = document.getElementById("modal-iframe");
+    modalIframe.src = "";
+    modal.style.display = "none";
+}
