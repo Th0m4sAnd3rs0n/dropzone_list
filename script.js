777c7a1dc8735e38a7bf1194b78e52ae4ea62d88
Th0m4sAnd3rs0n <cation-tubulaire0m@icloud.com>
2023-10-17 20:29:50 +0200

    Ajout d'une barre type barre de vie (test)

--- script.js
+++ script.js
@@ -28,3 +28,30 @@ function changeView(select) {
 }
 
 
+// Fonction pour mettre à jour la barre de vie en fonction de la valeur
+function updateBarreDeVie(valeur) {
+    // Mettre à jour la largeur du remplissage en fonction de la valeur
+    const barreVie = document.getElementById("barreVie");
+    barreVie.style.width = valeur + "%";
+
+    // Utiliser une échelle de couleurs en fonction de la valeur
+    if (valeur < 30) {
+        barreVie.style.backgroundColor = "red";
+    } else if (valeur < 50) {
+        // Jaune pour les valeurs intermédiaires
+        barreVie.style.backgroundColor = "yellow";
+    } else if (valeur < 70) {
+        // Jaune pour les valeurs intermédiaires
+        barreVie.style.backgroundColor = "blue";
+    } else {
+        // Vert pour les valeurs élevées
+        barreVie.style.backgroundColor = "green";
+    }
+
+}
+
+//----------------- lancé au chargement de la page --------------------
+
+document.addEventListener("DOMContentLoaded", updateBarreDeVie(30) );
+
+
